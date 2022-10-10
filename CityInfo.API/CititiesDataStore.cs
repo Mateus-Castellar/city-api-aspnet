983ea5c934a8ac3c6dd8c4301d0b1d0aaa364e2d
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-10 18:53:30 -0300

    feat: Model e dados mocks de cidades

---
+++ CityInfo.API/CititiesDataStore.cs
@@ -0,0 +1,41 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CititiesDataStore
+    {
+        public List<CityDTO> Citities { get; set; }
+        public static CititiesDataStore Current { get; set; } = new CititiesDataStore();
+
+        public CititiesDataStore()
+        {
+            Citities = new List<CityDTO>()
+            {
+                new CityDTO()
+                {
+                    Id = 1,
+                    Name ="Jaú",
+                    Description="Descricao da cidade"
+                },
+                new CityDTO()
+                {
+                    Id = 2,
+                    Name ="Itapui",
+                    Description="Descricao da cidade"
+                },
+                new CityDTO()
+                {
+                    Id = 3,
+                    Name ="Franca",
+                    Description="Descricao da cidade"
+                },
+                new CityDTO()
+                {
+                    Id = 4,
+                    Name ="Bariri",
+                    Description="Descricao da cidade"
+                },
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CititiesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/citities")]
+    public class CititiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCitites()
+        {
+            return new JsonResult(CititiesDataStore.Current.Citities);
+        }
+    }
+}

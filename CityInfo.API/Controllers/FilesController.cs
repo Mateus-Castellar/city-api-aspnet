532f429b3e970237db471750e4a954a36ef87d15
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-11 01:35:24 -0300

    feat: Controller para baixar arquivos txt e pdf

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileProvider;
+
+        public FilesController(FileExtensionContentTypeProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        [HttpGet("txt/{fileId}")]
+        public IActionResult GetFileSimple(string fileId)
+        {
+            var pathToFile = "fileApi.txt";
+
+            if (System.IO.File.Exists(pathToFile) is false)
+                return NotFound();
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, "text/plain", Path.GetFileName(pathToFile));
+        }
+
+        [HttpGet("pdf/{fileId}")]
+        public IActionResult GetFilePDF(string fileId)
+        {
+            var pathToFile = "apostila.pdf";
+
+            if (System.IO.File.Exists(pathToFile) is false)
+                return NotFound();
+
+            if (_fileProvider.TryGetContentType(pathToFile, out var contentType) is false)
+                contentType = "application/octet-stream";
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+    }
+}

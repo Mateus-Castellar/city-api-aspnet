5e490c001364edb92d1aba6cacd49b0c9e0b078a
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-11 16:52:01 -0300

    feat: Populando base de dados no contexto

--- CityInfo.API/Data/CityInfoContext.cs
+++ CityInfo.API/Data/CityInfoContext.cs
@@ -9,5 +9,47 @@ namespace CityInfo.API.Data
 
         public DbSet<City> Citys { get; set; } = null!;
         public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().HasData(
+                new City("São Paulo")
+                {
+                    Id = 1,
+                    Description = "Cidade localizada no Brazil"
+                },
+                new City("Belo Horizonte")
+                {
+                    Id = 2,
+                    Description = "Cidade localizada no Brazil"
+                },
+                new City("Porto Alegre")
+                {
+                    Id = 3,
+                    Description = "Cidade localizada no Brazil"
+                });
+
+            modelBuilder.Entity<PointOfInterest>().HasData(
+                new PointOfInterest("Neo Quimica Arena")
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Description = "Estadio de futebol"
+                },
+                new PointOfInterest("Mineirão")
+                {
+                    Id = 2,
+                    CityId = 2,
+                    Description = "Estadio de futebol"
+                },
+                new PointOfInterest("Beira Rio")
+                {
+                    Id = 3,
+                    CityId = 3,
+                    Description = "Estadio de futebol"
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ CityInfo.API/Migrations/20221011195114_SeedTables.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfo.API.Migrations
+{
+    public partial class SeedTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "Cidade localizada no Brazil", "São Paulo" });
+
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "Cidade localizada no Brazil", "Belo Horizonte" });
+
+            migrationBuilder.InsertData(
+                table: "Citys",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "Cidade localizada no Brazil", "Porto Alegre" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 1, 1, "Estadio de futebol", "Neo Quimica Arena" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 2, 2, "Estadio de futebol", "Mineirão" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 3, 3, "Estadio de futebol", "Beira Rio" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Citys",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Citys",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Citys",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
